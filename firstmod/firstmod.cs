ce1fa146a41e5a10446889af9392839101eda0bb
olofens <olofens@student.chalmers.se>
2018-08-06 00:49:48 +0200

    testing how to add item. not completed

--- firstmod/firstmod.cs
+++ firstmod/firstmod.cs
@@ -10,15 +10,21 @@ using UnityEngine;
 [RaftVersion("1.03")]
 public class firstmod : Mod
 {
+
+    private ModItem newItem;
+
     private void Start()
     {
+        newItem = ScriptableObject.CreateInstance<ModItem>().Create("WindTurbine", ItemType.Inventory);
+        newItem.setInventorySettings("Wind Turbine", "A wind turbine which can be repurposed in several ways.", null, "Item/" + newItem.name, 5);
+        RAPI.addItem(newItem);
         RConsole.Log("firstmod loaded!");
     }
 
 
     public void Update()
     {
-        
+        Network_Player player = RAPI.getLocalPlayer();
     }
 
     public void OnModUnload()
